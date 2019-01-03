8a621ceb72b962c623eb93fd683cd451aeab831a
kcui5 <kcui@ucvts.org>
2019-01-03 15:13:10 +0000

    Started testing pyglet

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -5,3 +5,12 @@ import Drawer.py
 import ImageAnalyzer.py
 
 window = pyglet.window.Window()
+titleTextOne = pyglet.text.Label('Kyle\'s', font_size = 24, x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center')
+titleTextTwo = pyglet.text.Label('Quick, Draw!', font_size = 36, x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center')\
+
+def on_draw():
+    window.clear()
+    titleTextOne.draw()
+    titleTextTwo.draw()
+
+pyglet.app.run()
